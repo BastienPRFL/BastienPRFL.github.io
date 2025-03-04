4a91a3b4f68b6f1bde3897c22ed43f828cbccb24
Bastien PERRUFFEL <bastienperruffel@gmail.com>
2025-03-04 19:59:05 +0100

    progression2

---
+++ competences.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <link rel="stylesheet" href="style/competences.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/flatly/bootstrap.min.css" integrity="sha512-qoT4KwnRpAQ9uczPsw7GunsNmhRnYwSlE2KRCUPRQHSkDuLulCtDXuC2P/P6oqr3M5hoGagUG9pgHDPkD2zCDA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <?php include 'navbar.html'; ?>
+    
+    <section id="presentation">
+        <div><h1><b>Mes différentes compétences</b></h1></div>
+        <div class="competences">
+            <div>
+                <p>La 3e année de BUT Informatique comporte 3 compétences dans le parcours A : Réalisation d'applications : conception, développement, validation :</p>
+            </div>
+            <div class="comp-title">Compétence 1 : Réaliser un développement d'application <br>
+            Compétence 2 : Optimiser des applications informatique <br>
+            Compétence 3 : Travailler dans une équipe informatique</div>
+        </div>
+    </section>
+    
+    <section id="comp1">
+        <div class="card text-white bg-primary">
+            <div class="card-header">Compétence 1</div>
+            <div class="card-body">
+                <h4 class="card-title">Réaliser un développement d'application</h4>
+                <p class="card-text">
+                    Développer — c'est-à-dire concevoir, coder, tester et intégrer — une solution informatique pour un client<br>
+                    – en respectant les besoins décrits par le client<br>
+                    – en appliquant les principes algorithmiques<br>
+                    – en veillant à la qualité du code et à sa documentation<br>
+                    – en choisissant les ressources techniques appropriées<br>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section id="comp2">
+        <div class="card text-white bg-primary">
+            <div class="card-header">Compétence 2</div>
+            <div class="card-body">
+                <h4 class="card-title">Optimiser des applications informatique</h4>
+                <p class="card-text">
+                    Proposer des applications informatiques optimisées en fonction de critères spécifiques : temps d'exécution, précision, consommation de ressources…<br>
+                    – en formalisant et modélisant des situations complexes<br>
+                    – en recensant les algorithmes et les structures de données usuels<br>
+                    – en s'appuyant sur des schémas de raisonnement<br>
+                    – en justifiant les choix et validant les résultats<br>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section id="comp3">
+        <div class="card text-white bg-primary">
+            <div class="card-header">Compétence 3</div>
+            <div class="card-body">
+                <h4 class="card-title">Travailler dans une équipe informatique</h4>
+                <p class="card-text">
+                    Acquérir, développer et exploiter les aptitudes nécessaires pour travailler efficacement dans une équipe informatique.<br>
+                    – en inscrivant sa démarche au sein d'une équipe pluridisciplinaire<br>
+                    – en accompagnant la mise en œuvre des évolutions informatiques<br>
+                    – en veillant au respect des contraintes juridiques<br>
+                    – en développant une communication efficace et collaborative<br>
+                </p>
+            </div>
+            <div class="card-footer">
+                J'ai pu 
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
